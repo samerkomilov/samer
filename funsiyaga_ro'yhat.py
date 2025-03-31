5667d5ec7cee7fd6bbcb4acbc3b233e3fe1ac8ce
samerkomilov <samerkomilov1876@gmail.com>
2025-03-31 09:17:12 +0500

    Update funsiyaga_ro'yhat.py

--- funsiyaga_ro'yhat.py
+++ funsiyaga_ro'yhat.py
@@ -33,7 +33,18 @@ bahola=bahola(talaba)
 print(bahola)
 print(talaba)
 
+talabalar = ['ali', 'vali', 'hasan', 'husan']
 
+def bahola(ismlar):
+    baholar = {}
+    for ism in ismlar:
+        baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+        baholar[ism]=baho
+    return baholar
+        
+baholar = bahola(talabalar)
+print(baholar)
+print(talabalar)
 
 
 
